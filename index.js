1eaced03b9b53e6d0be4ba63bf34ef635811e9a2
Leanne Baldwin <leanne@reefbreak.co.uk>
2017-01-10 16:40:36 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+exports.handler = function (event, context) {
+    try {
+        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+      
+      
+if (event.session.application.applicationId !== "amzn1.ask.skill.99d85a48-aaf7-4552-b47b-990350016e92") {
+  context.fail("Invalid Application ID");
+}
