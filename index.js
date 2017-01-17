8a5f2ad625c7f6d8a169f7c81b1225d983e99426
Leanne Baldwin <leanne@reefbreak.co.uk>
2017-01-17 13:14:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,377 @@
+'use strict';
 
+var questions = [
+    {
+        "What is" + firstnumber.toString() + "multiplied by" + secondnumber.toString() : [
+            correctanswer
+        ]
+    },
+    {
+        "What is 6 multiplied by 3?:": [
+            "18"
+        ]
+    },
+    {
+        "What is 4 multiplied by 8?": [
+            "32"
+        ]
+    },
+    {
+        "What is 11 multiplied by 12?": [
+            "132"
+        ]
+    },
+    {
+        "What is 2 multiplied by 2?": [
+            "2"
+        ]
+    },
+    {
+        "What is 7 multiplied by 8?": [
+            "56"
+        ]
+    },
+];
+
+exports.handler = function (event, context) {
+    try {
+        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+
+  if (event.session.application.applicationId !== "amzn1.ask.skill.034c99ec-e55e-4c6a-89ec-b4a29c6812b2") {
+      context.fail("Invalid Application ID");
+  }
+
+        if (event.session.new) {
+            onSessionStarted({requestId: event.request.requestId}, event.session);
+        }
+
+        if (event.request.type === "LaunchRequest") {
+            onLaunch(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "IntentRequest") {
+            onIntent(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "SessionEndedRequest") {
+            onSessionEnded(event.request, event.session);
+            context.succeed();
+        }
+    } catch (e) {
+        context.fail("Exception: " + e);
+    }
+};
+
+function onSessionStarted(sessionStartedRequest, session) {
+    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+}
+
+
+function onLaunch(launchRequest, session, callback) {
+    console.log("onLaunch requestId=" + launchRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    getWelcomeResponse(callback);
+}
+
+function onIntent(intentRequest, session, callback) {
+    console.log("onIntent requestId=" + intentRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    var intent = intentRequest.intent,
+        intentName = intentRequest.intent.name;
+
+    if (session.attributes && session.attributes.userPromptedToContinue) {
+        delete session.attributes.userPromptedToContinue;
+        if ("AMAZON.NoIntent" === intentName) {
+            handleFinishSessionRequest(intent, session, callback);
+        } else if ("AMAZON.YesIntent" === intentName) {
+            handleRepeatRequest(intent, session, callback);
+        }
+    }
+
+    if ("AnswerIntent" === intentName) {
+        handleAnswerRequest(intent, session, callback);
+    } else if ("AnswerOnlyIntent" === intentName) {
+        handleAnswerRequest(intent, session, callback);
+    } else if ("DontKnowIntent" === intentName) {
+        handleAnswerRequest(intent, session, callback);
+    } else if ("AMAZON.YesIntent" === intentName) {
+        handleAnswerRequest(intent, session, callback);
+    } else if ("AMAZON.NoIntent" === intentName) {
+        handleAnswerRequest(intent, session, callback);
+    } else if ("AMAZON.StartOverIntent" === intentName) {
+        getWelcomeResponse(callback);
+    } else if ("AMAZON.RepeatIntent" === intentName) {
+        handleRepeatRequest(intent, session, callback);
+    } else if ("AMAZON.HelpIntent" === intentName) {
+            handleGetHelpRequest(intent, session, callback);
+    } else if ("AMAZON.StopIntent" === intentName) {
+        handleFinishSessionRequest(intent, session, callback);
+    } else if ("AMAZON.CancelIntent" === intentName) {
+        handleFinishSessionRequest(intent, session, callback);
+    } 
+}
+
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+}
+
+var ANSWER_COUNT = 1;
+var GAME_LENGTH = 5;
+var CARD_TITLE = "Vital Math"; 
+
+function getWelcomeResponse(callback) {
+   
+    var sessionAttributes = {},
+       
+        speechOutput = "I will ask you " + GAME_LENGTH.toString()
+            + " math questions, try to get as many right as you can. Just say the answer. Let's begin. ",
+        shouldEndSession = false,
+
+        gameQuestions = populateGameQuestions(),
+        correctAnswerIndex = Math.floor(Math.random() * (ANSWER_COUNT)), // Generate a random index for the correct answer, from 0 to 3
+        roundAnswers = populateRoundAnswers(gameQuestions, 0, correctAnswerIndex),
+
+        currentQuestionIndex = 0,
+        spokenQuestion = Object.keys(questions[gameQuestions[currentQuestionIndex]]),
+        repromptText = spokenQuestion,
+
+        i, j;
+
+    for (i = 0; i < ANSWER_COUNT; i++) {
+        repromptText += "";
+    }
+    speechOutput += repromptText;
+    sessionAttributes = {
+        "speechOutput": repromptText,
+        "repromptText": repromptText,
+        "currentQuestionIndex": currentQuestionIndex,
+        "correctAnswerIndex": correctAnswerIndex + 1,
+        "questions": gameQuestions,
+        "score": 0,
+        "correctAnswerText":
+            questions[gameQuestions[currentQuestionIndex]][Object.keys(questions[gameQuestions[currentQuestionIndex]])[0]][0]
+    };
+    callback(sessionAttributes,
+        buildSpeechletResponse(CARD_TITLE, speechOutput, repromptText, shouldEndSession));
+}
+
+function populateGameQuestions() {
+    var gameQuestions = [];
+    var indexList = [];
+    var index = questions.length;
+
+    if (GAME_LENGTH > index){
+        throw "Invalid Game Length.";
+    }
+
+    for (var i = 0; i < questions.length; i++){
+        indexList.push(i);
+    }
+
+    for (var j = 0; j < GAME_LENGTH; j++){
+        var rand = Math.floor(Math.random() * index);
+        index -= 1;
+
+        var temp = indexList[index];
+        indexList[index] = indexList[rand];
+        indexList[rand] = temp;
+        gameQuestions.push(indexList[index]);
+    }
+
+    return gameQuestions;
+}
+
+function populateRoundAnswers(gameQuestionIndexes, correctAnswerIndex, correctAnswerTargetLocation) {
+    
+    var answers = [],
+        answersCopy = questions[gameQuestionIndexes[correctAnswerIndex]][Object.keys(questions[gameQuestionIndexes[correctAnswerIndex]])[0]],
+        temp, i;
+
+    var index = answersCopy.length;
+
+    if (index < ANSWER_COUNT){
+        throw "Not enough answers for question.";
+    }
+
+    for (var j = 1; j < answersCopy.length; j++){
+        var rand = Math.floor(Math.random() * (index - 1)) + 1;
+        index -= 1;
+
+        temp = answersCopy[index];
+        answersCopy[index] = answersCopy[rand];
+        answersCopy[rand] = temp;
+    }
+
+    for (i = 0; i < ANSWER_COUNT; i++) {
+        answers[i] = answersCopy[i];
+    }
+    temp = answers[0];
+    answers[0] = answers[correctAnswerTargetLocation];
+    answers[correctAnswerTargetLocation] = temp;
+    return answers;
+}
+
+function handleAnswerRequest(intent, session, callback) {
+    var speechOutput = "";
+    var sessionAttributes = {};
+    var gameInProgress = session.attributes && session.attributes.questions;
+    var answerSlotValid = isAnswerSlotValid(intent);
+    var userGaveUp = intent.name === "DontKnowIntent";
+
+    if (!gameInProgress) {
+        sessionAttributes.userPromptedToContinue = true;
+        speechOutput = "There is no game in progress. Do you want to start a new game? ";
+        callback(sessionAttributes,
+            buildSpeechletResponse(CARD_TITLE, speechOutput, speechOutput, false));
+    } else {
+        if (!answerSlotValid && !userGaveUp) {
+        var reprompt = session.attributes.speechOutput;
+        speechOutput = "Sorry, your answer is not is our list. " + reprompt;
+        callback(session.attributes,
+            buildSpeechletResponse(CARD_TITLE, speechOutput, reprompt, false));
+        } else {
+        var gameQuestions = session.attributes.questions,
+            correctAnswerIndex = parseInt(session.attributes.correctAnswerIndex),
+            currentScore = parseInt(session.attributes.score),
+            currentQuestionIndex = parseInt(session.attributes.currentQuestionIndex),
+            correctAnswerText = session.attributes.correctAnswerText;
+
+        var speechOutputAnalysis = "";
+        
+
+        if (answerSlotValid && intent.slots.Answer.value.toUpperCase() == correctAnswerText.toUpperCase()) {
+            currentScore++;
+            speechOutputAnalysis = "correct. ";
+        } else {
+            if (!userGaveUp) {
+                speechOutputAnalysis = "wrong. ";
+            }
+            speechOutputAnalysis += "The correct answer is " + correctAnswerText + ". ";
+        }
+        if (currentQuestionIndex == GAME_LENGTH - 1) {
+            speechOutput = userGaveUp ? "" : "That answer is ";
+            speechOutput += speechOutputAnalysis + "You got " + currentScore.toString() + " out of "
+                + GAME_LENGTH.toString() + " questions correct. Thank you for learning with Leanne!";
+            callback(session.attributes,
+                buildSpeechletResponse(CARD_TITLE, speechOutput, "", true));
+        } else {
+            currentQuestionIndex += 1;
+            var spokenQuestion = Object.keys(questions[gameQuestions[currentQuestionIndex]]);
+            correctAnswerIndex = Math.floor(Math.random() * (ANSWER_COUNT));
+            var roundAnswers = populateRoundAnswers(gameQuestions, currentQuestionIndex, correctAnswerIndex),
+
+                questionIndexForSpeech = currentQuestionIndex + 1,
+                repromptText =  spokenQuestion ;
+            for (var i = 0; i < ANSWER_COUNT; i++) {
+                repromptText +=  "";
+            }
+            speechOutput += userGaveUp ? "" : "That answer is ";
+            speechOutput += speechOutputAnalysis + "Your score is " + currentScore.toString() + ". " + repromptText;
+
+            sessionAttributes = {
+                "speechOutput": repromptText,
+                "repromptText": repromptText,
+                "currentQuestionIndex": currentQuestionIndex,
+                "correctAnswerIndex": correctAnswerIndex + 1,
+                "questions": gameQuestions,
+                "score": currentScore,
+                "correctAnswerText":
+                    questions[gameQuestions[currentQuestionIndex]][Object.keys(questions[gameQuestions[currentQuestionIndex]])[0]][0]
+            };
+            callback(sessionAttributes,
+                buildSpeechletResponse(CARD_TITLE, speechOutput, repromptText, false));
+        }
+    }
+    }
+}
+
+function handleRepeatRequest(intent, session, callback) {
+    if (!session.attributes || !session.attributes.speechOutput) {
+        getWelcomeResponse(callback);
+    } else {
+        callback(session.attributes,
+            buildSpeechletResponseWithoutCard(session.attributes.speechOutput, session.attributes.repromptText, false));
+    }
+}
+
+function handleGetHelpRequest(intent, session, callback) {
+    if (session.attributes) {
+        session.attributes.userPromptedToContinue = true;
+    } else {
+        session.attributes = { userPromptedToContinue: true };
+    }
+
+    var speechOutput = "To start a new math game at any time, say, start new game. "
+        + "To repeat the last element, say, repeat. "
+        + "Would you like to keep playing?",
+        repromptText = "Try to get the right answer. "
+        + "Would you like to keep playing?";
+        var shouldEndSession = false;
+    callback(session.attributes,
+        buildSpeechletResponseWithoutCard(speechOutput, repromptText, shouldEndSession));
+}
+
+function handleFinishSessionRequest(intent, session, callback) {
+    callback(session.attributes,
+        buildSpeechletResponseWithoutCard("Thanks for playing Math Test!", "", true));
+}
+
+function isAnswerSlotValid(intent) {
+    var answerSlotFilled = intent.slots && intent.slots.Answer && intent.slots.Answer.value;
+    var answerSlotIsInt = answerSlotFilled && !isNaN(parseInt(intent.slots.Answer.value));
+    return 1;
+}
+
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        card: {
+            type: "Simple",
+            title: title,
+            content: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildSpeechletResponseWithoutCard(output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: "1.0",
+        sessionAttributes: sessionAttributes,
+        response: speechletResponse
